1ff1cc8caf879dd795cb1521afd5e1754741b3c2
Ziyawwd <114824886+Ziyawwd@users.noreply.github.com>
2023-02-28 21:05:12 +0400

    Update Тест BOOM.js

--- Тест BOOM.js
+++ Тест BOOM.js
@@ -1,58 +1,58 @@
 /* 1)
 //выведите в консоли ширину и высоту окна браузера с помощью window
-console.log(window.width)
-console.log(window.height)
+console.log(window.innnerWidth)
+console.log(window.innerHeight)
 2)
 //создайте обработчик события для кнопки ниже, которая будет открывать новое окно в браузере при нажатии
 <button id='open'>Open</button>
-document.addEventListener(#open, a)
+document.addEventListener(click, a)
 function a() {
     window.open()
 }
 3)
 //создайте обработчик события для кнопки ниже, которая будет закрывать текущее окно в браузере при нажатии
 <button id='close'>Open</button>
-document.addEventListener(#close, a)
+document.addEventListener(click, a)
 function a() {
     window.open()
 }
 4)
 //создайте обработчик события для кнопки ниже, которая будет перемещать текущее окно в браузере при нажатии
 <button id='move'>Open</button>
-document.addEventListener(#move, a)
+document.addEventListener(click, a)
 function a() {
     window.move()
 }
 5)
 //создайте обработчик события для кнопки ниже, которая будет менять размер текущего окно в браузере при нажатии
 <button id='remove'>Open</button>
-document.addEventListener(#remove, a)
+document.addEventListener(click, a)
 function a() {
     window.remove()
 }
 6)
 //выведите в консоли ширину экрана пользователя в пикселях
-console.log(window.Innerwidth())
+console.log(screen.innerWidth())
 7)
 //выведите в консоли высоту экрана пользователя в пикселях
-console.log(window.Innerheight())
+console.log(screen.innerHeight())
 8)
 //выведите в консоли ширину экрана пользователя без панели задач и интерфейса вкладок в пикселях
-6
+console.log(screen.Width())
 9)
 //выведите в консоли высоту экрана пользователя без панели задач и интерфейса вкладок в пикселях
-7
+console.log(screen.height())
 10)
 //выведите в консоли сколько битов использует браузер пользователя
 не знаю
 11)
 //выведите в консоли глубину экрана пользователя в пикселях
-console.log(screen.ColorDeph)
+console.log(screen.ColorDepth)
 12)
 //создайте обработчик события для кнопки ниже, которая будет перекидывать на предыдущую страницу в браузере при нажатии
 <button id='back'>Open</button>
 
-document.addEvenetLisener('#back', ads)
+document.addEvenetLisener(click, ads)
 function a() {
     return window.back()
 
@@ -60,7 +60,7 @@ function a() {
 13)
 //создайте обработчик события для кнопки ниже, которая будет перекидывать на следующую страницу в браузере при нажатии
 <button id='next'>Open</button>
-document.addEvenetLisener('#next', ads)
+document.addEvenetLisener(click, ads)
 function a() {
     return window.next()
 
@@ -76,19 +76,19 @@ navigator.appName
 navigator.appCodeName
 17)
 //с помощью какого свойства можно проверить какая версия у браузера пользователя
-appVersion
+appVersion()
 18)
 //с помощью какого свойства можно отправить заголовок браузера на сервер
-userAgent
+userAgent()
 19)
 //с помощью какого свойства можно узнать операционную систему пользователя
-Platform
+Platform()
 20)
 //с помощью какого свойства можно узнать язык браузера пользователя
-Language
+Language()
 21)
 //с помощью какого свойства можно узнать находится в данный момент пользователь в сети или нет
-online
+online()
 22)
 //с помощью какого свойства можно проверить включен ли java у пользователя
 javaEnabled() 
